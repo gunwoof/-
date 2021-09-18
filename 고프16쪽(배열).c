c43575b75ad51dbcb48c496a6654fb1eec25efd2
gunwoof <gunwoof1234@naver.com>
2021-09-18 17:57:08 +0900

    고급프로그래밍

--- 고프16쪽(배열).c
+++ 고프16쪽(배열).c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #define Len 10
 
-int main(void) {
+int main_array4(void) {
 	int i;
 	int ele[Len]={3,2,0,1,-4,10,-5,5,-1,9};
-	int bound =3;
+	int answer1;
+	int answer2;
+	int sum =0;
+	
 	
 	
 	// index를 2, 3, 4, 5, 6, 7, 8, 9, 0, 1순서로 하여 배열원소 출력  
@@ -12,7 +15,31 @@ int main(void) {
 		printf("%d ", ele[i+2 % 10]);
 	}
 	
-	// Practice 1) 
+	/*// Practice 1) 2개의 정수를 입력받고, 2개 정수 사이의 원소를 출력 (index값 사이인지, 실제 입력값사이 인지 모호하다)
+	// -> 여기서는 실제 입력값 사이를 구한다 
+	scanf_s("%d %d", &answer1, &answer2);
+	for(i=0; i < Len; i++) {
+		if(answer1 < ele[i] && ele[i] < answer2) {
+			printf("%d ", ele[i]);
+		}
+	}
+	
+	// Practice 2) 2개의 정수를 입력받고, 2개 정수 사이의 원소를 더하여 출력 (index값 사이인지, 실제 입력값사이 인지 모호하다)
+	// -> 여기서는 실제 입력값 사이를 구한다
+	scanf_s("%d %d", &answer1, &answer2);
+	for(i=0; i < Len; i++) {
+		if(answer1 < ele[i] && ele[i] < answer2) {
+			sum += ele[i];
+			
+		}
+	}
+	printf("%d", sum);
+	
+	// Practice 3) index 0부터 한 index를 뛴 배열 원소의 합을 출력 
+	for(i = 0; i < Len; i=i+2) {
+		sum += ele[i];
+	}
+	printf("%d", sum);*/
 }
 
 
---
+++ 고프17쪽(배열).c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#define Len 10
+
+int main(void) {
+	int i;
+	int ele[Len]={3,2,0,1,-4,10,-5,5,-1,9}; 
+	int sum =0;
+	
+	/*// 배열원소 중 제일 큰 원소를 출력 
+	int max = -100;
+	
+	for (i=0; i < Len; i++) {
+		if(max < ele[i]) {
+			max = ele[i];
+		}	
+	}
+	printf("Max %d\n", max);
+	
+	// Practice 0) 모든 배열원소를 더한 것을 출력 
+	for (i=0; i < Len; i++) {
+		sum += ele[i]	
+	}
+	printf("%d\n", sum);
+	
+	// Practice 1) 배열원소 중 제일 작은 원소를 출력 
+	int minimum = 100;
+	
+	for (i=0; i < Len; i++) {
+		if(minimum > ele[i]) {
+			minimum = ele[i];
+		}	
+	}
+	printf("Minimum %d\n", minimum);
+	
+	// Practice 2) 배열원소 중 제일 큰 원소의 index를 출력
+	int max = -100;
+	int maxi; // 제일 큰 원소의 index 
+	
+	for (i=0; i < Len; i++) {
+		if(max < ele[i]) {
+			max = ele[i];
+			maxi = i;
+		}	
+	}
+	printf("%d\n", maxi);*/
+	
+	// Practice 3) 배열원소 중 제일 큰 원소와 두번째로 큰 원소를 출력 
+	int max = -100;
+	int maxi; // 제일 큰 원소의 index
+	
+	for (i=0; i < Len; i++) {
+		if(max < ele[i]) {
+			max = ele[i];
+			maxi = i; 
+		}	
+	}
+	printf("Max %d\n", max);
+	ele[maxi] = -100; // 가장 큰 수를 가장 작을 수로 만들기 
+	
+	for (i=0; i < Len; i++) {
+		if(max < ele[i]) {
+			max = ele[i]; 
+			maxi = i;
+		}	
+	}
+	printf("Max %d\n", max); // 두 번째로 큰 원소 출력 
+	
+	
+	
+}
