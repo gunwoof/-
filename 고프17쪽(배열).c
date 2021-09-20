f1d4ddef8f29f39b2002e451cfd2d00f0e1d0594
gunwoof <gunwoof1234@naver.com>
2021-09-20 21:53:06 +0900

    고급프로그래밍

--- 고프17쪽(배열).c
+++ 고프17쪽(배열).c
@@ -6,7 +6,7 @@ int main(void) {
 	int ele[Len]={3,2,0,1,-4,10,-5,5,-1,9}; 
 	int sum =0;
 	
-	/*// 배열원소 중 제일 큰 원소를 출력 
+	// 배열원소 중 제일 큰 원소를 출력 
 	int max = -100;
 	
 	for (i=0; i < Len; i++) {
@@ -16,7 +16,7 @@ int main(void) {
 	}
 	printf("Max %d\n", max);
 	
-	// Practice 0) 모든 배열원소를 더한 것을 출력 
+	/*// Practice 0) 모든 배열원소를 더한 것을 출력 
 	for (i=0; i < Len; i++) {
 		sum += ele[i]	
 	}
@@ -42,7 +42,7 @@ int main(void) {
 			maxi = i;
 		}	
 	}
-	printf("%d\n", maxi);*/
+	printf("%d\n", maxi);
 	
 	// Practice 3) 배열원소 중 제일 큰 원소와 두번째로 큰 원소를 출력 
 	int max = -100;
@@ -57,13 +57,15 @@ int main(void) {
 	printf("Max %d\n", max);
 	ele[maxi] = -100; // 가장 큰 수를 가장 작을 수로 만들기 
 	
+	max = -100; // max를 재정의 안해주면 max값은 10인 상태로 코드 진행...
+	
 	for (i=0; i < Len; i++) {
 		if(max < ele[i]) {
 			max = ele[i]; 
 			maxi = i;
 		}	
 	}
-	printf("Max %d\n", max); // 두 번째로 큰 원소 출력 
+	printf("Max %d\n", max); // 두 번째로 큰 원소 출력 */
 	
 	
 	
