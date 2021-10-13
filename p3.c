9fbaeb4a031f45649691ca9e00e64149bcfc6dff
gunwoof <gunwoof1234@naver.com>
2021-10-13 19:31:56 +0900

    고급프로그래밍

--- p3.c
+++ p3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main_2(void) {
+int main(void) {
 	int Score[6];
 	printf("x, y, z와 맞은 문제의 수를 술서대로 입력하시오!\n");
 	scanf_s("%d %d %d %d %d %d", &Score[0], &Score[1], &Score[2], &Score[3], &Score[4], &Score[5]);
@@ -12,12 +12,12 @@ int main_2(void) {
 			if(Score[3] == Score[0]) {
 				printf("A"); exit(0); 
 			}
-			else if(Score[0]/2 <= Score[3] && Score[3] <Score[0]) {
+			else if((double)Score[0]/2 <= Score[3] && Score[3] <Score[0]) {
 				printf("B"); exit(0);
 			}
-			printf("C");
+			printf("C"); exit(0);
 		}
-		else if(Score[1]/2 <= Score[4] && Score[4] <Score[1]) {
+		else if((double)Score[1]/2 <= Score[4] && Score[4] <Score[1]) {
 			printf("D"); exit(0);
 		}
 		printf("E"); exit(0);
