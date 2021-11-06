a79bfcfc7b6a5c95eca18af762cfd6c801919ed3
gunwoof <gunwoof1234@naver.com>
2021-11-06 17:02:55 +0900

    고급프로그래밍

--- 고프54~55쪽(재귀함수).c
+++ 고프54~55쪽(재귀함수).c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 void hanoi(char from, char mid, char to, int n); // 하노이 타워(3이상)
-void hanoix(char from, char mid, char to, int n); // 하노이 타워(3이상)
+void hanoiA(char from, char mid, char to, int n); // 'A'로부터 출발한 움직임을 세어서 출력 
+void hanoix(char from, char mid, char to, int n); //  n=5, 'A'에 3개의 폴대를 가질때의 장면을 새서 출력
 
+int count =0;
 
-int main_recursion3(void) {
+int main/*_recursion3*/(void) {
 	
-	// 하노이 타워 출력 
-	hanoi('A','B','c',3);
+/*	// 하노이 타워 출력 
+	hanoi('A','B','c',3);*/
 	
-	// 'A'로부터 출발한 움직을을 새서 출력
+	// 'A'로부터 출발한 움직임을 세어서 출력
+	hanoiA('A','B','C',3);
+	printf("%d",count);
 	
-	// n=5, 'A'에 3개의 폴대를 가질때의 장면을 새서 출력 
-	hanoix('A','B','C',5);
-	  
+/*	// n=5, 'A'에 3개의 폴대를 가질때의 장면을 새서 출력 
+	hanoix('A','B','c',5);
+	printf("%d",count);*/
+	
 	
 }
 
@@ -30,10 +35,31 @@ void hanoi(char from, char mid, char to, int n) {
 	}
 }
 
-void hanoix(char from, char mid, char to, int n) {
-	int count[3] = {5,0,0};
+void hanoiA(char from, char mid, char to, int n) {
 	if(n == 1) {
-		
+		if(from == 'a') {
+			count++;
+			
+		} 
+		printf("move from %c to %c\n", from, to);
+		return;
+	} 
+	else {
+		hanoi(from, to, mid, n-1);
+		hanoi(from, mid, to, 1);
+		hanoi(mid, from, to, n-1);
+		return;
+	}
+}
+
+void hanoix(char from, char mid, char to, int n) {
+	int countpole[3] = {5,0,0};
+	if(n == 1) {
+		countpole[from - 'A']--;
+		countpole[to - 'A']++;
+		if(countpole[0] == 3) {
+			count++;
+		}
 		printf("move from %c to %c\n", from, to);
 		return;
 	} 
