a8572f596b009da5a71ed7f843c2fec8103a819f
gunwoof <gunwoof1234@naver.com>
2022-01-07 16:38:51 +0900

    고급프로그래밍

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int IsPrime(int n);
 double square(double x, int y);
 
-int main(int argc, char** argv) {
+int main_1(int argc, char** argv) {
 	int u;
 	for( u=2; u <10; u++) {
 		printf("%d %d\n", u, IsPrime(u));
